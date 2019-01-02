b4edb0cb213218cdc59ed9952bd143bfed431731
Simon Schoof <s.schoof@fielmann.com>
2019-01-02 17:16:41 +0100

    added stalonetray, xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,10 +4,16 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 
-main = xmonad =<< xmobar desktopConfig
-    { terminal    = "urxvt"
-    , modMask     = mod4Mask
-    , startupHook = setWMName "LG3D"
-    , layoutHook  = avoidStruts $ layoutHook defaultConfig
-    , manageHook  = manageHook defaultConfig <+> manageDocks
+main = do
+    xmonad =<< xmobar desktopConfig
+    { terminal    = myTerminal
+    , modMask     = myModMask
+    , startupHook = myStartUpHook
+    , layoutHook  = avoidStruts $ layoutHook desktopConfig
+    , manageHook  = manageHook desktopConfig <+> manageDocks
     }
+
+myTerminal = "urxvt"
+myModMask = mod4Mask
+myBorderWidth = 3
+myStartUpHook = setWMName "LG3D"
