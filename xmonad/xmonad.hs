036119474a618a80b06b409b5777368b8ea37d05
Simon Schoof <s.schoof@fielmann.com>
2019-02-16 20:52:26 +0100

    add workspacenames

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,12 +8,14 @@ main =
     xmonad =<< xmobar desktopConfig
     { terminal    = myTerminal
     , modMask     = myModMask
+    , workspaces  = myWorkspaces
     , startupHook = myStartUpHook
     , layoutHook  = avoidStruts $ layoutHook desktopConfig
     , manageHook  = manageHook desktopConfig <+> manageDocks
     }
 
-myTerminal = "urxvt"
+myTerminal = "gnome-terminal"
 myModMask = mod4Mask
 myBorderWidth = 3
 myStartUpHook = setWMName "LG3D"
+myWorkspaces = ["1:Dev(IJ)","2:Dev(VS)","3:Web","4:Term","5","6","7:Keys","8:Mail","9:Chat"]
